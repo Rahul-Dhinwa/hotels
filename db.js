b8a2bcce4d43b20cd10788984ebffef4a6a9f142
Rahul-Dhinwa <rahuldhinwa26@gmail.com>
2025-01-18 23:49:59 +0530

    shift mingodb to online server

--- db.js
+++ db.js
@@ -1,21 +1,29 @@
 // For conectivity of database with mongodb
 
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 //define MongoDB coonnebtion URL
 
-const mongoURL = 'mongodb://localhost:27017/hotels'
+//const mongoURL = process.env.MONGODB_URL_LOCAL;
+const mongoURL = process.env.MONGODB_URL;
+
 
 //set mongoDB connection
-mongoose.connect(mongoURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+// mongoose.connect(mongoURL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// });
+
+mongoose.connect(mongoURL)
+    .then(() => console.log('Successfully connected to MongoDB'))
+    .catch((error) => console.error('MongoDB connection error:', error));
+
 
 //Get the default connectiion
 //Mongoose maintains a default connection Object representing the mongoDB connection.
-
 const db = mongoose.connection;
+
 
 //define event lisner for data base connection
 
@@ -23,8 +31,8 @@ db.on('connected', ()=>{
     console.log('connected to mongodb server');
 });
 
-db.on('error', ()=>{
-    console.log('mongodb connection error: ', error);
+db.on('error', (error) => { // 'error' parameter added here
+    console.log('MongoDB connection error:', error);
 });
 
 db.on('disconnected', ()=>{
